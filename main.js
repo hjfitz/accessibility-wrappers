7b278df439e7c2ea14edf0d9dacb1f9fe344d4ea
Harry <fitzgerald_harry@network.lilly.com>
2017-10-05 15:15:51 +0100

    add spaghetti

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-
-const synth = window.speechSynthesis;
-  
-
-const initRecog = () => {
+const initRecog = cb => {
 
   const commands = {
     'refresh page': () => { return window.location = window.location; },
     'open YouTube': () => { return window.open('https://www.youtube.com'); },
     'open Launchpad': () => { return window.open('https://www.cirr-lpld.herokuapp.com'); },
     'never going to give you up': () => { return window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); },
-    'Never Going To Give You Up': () => { return window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); },  
+    'Never Going to Give You Up': () => { return window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); },  
     'Never Gonna Give You Up': () => { return window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); },
-    'Phil': () => { synth.speak(new SpeechSynthesisUtterance('phil is a shit b a'))},
-    'Peter': () => { synth.speak(new SpeechSynthesisUtterance('please no'))}    
+    'Phil': () => { cb('phil is a shit b a')},
+    'Peter': () => { cb('please no')},
+    'Sam': () => { cb('do you want to go to starbucks')}
     
   }
 
@@ -21,7 +18,8 @@ const initRecog = () => {
   recognition.interimResults = true;
   recognition.lang = 'en-US';
 
-  let p = document.createElement('p');
+  let p = document.createElement('li');
+  p.classList = 'collection-item';
   const words = document.getElementById('content');
   words.appendChild(p);
   recognition.addEventListener('result', e => {
@@ -38,26 +36,55 @@ const initRecog = () => {
         Object.keys(commands).forEach(command => {
           if (transcript.match(command)) commands[command]()
         });
-        p = document.createElement('p');
-        words.prepend(p);
+        p = document.createElement('li');
+        p.classList = 'collection-item';
+        words.appendChild(p);
       }
   });
   recognition.addEventListener('end', recognition.start);
   recognition.start();
 }
 
-const speechSynth = () => {
-  const area = document.querySelector('textarea');
-  const button = document.getElementById('speak');
-  button.addEventListener('click', () => {
-    const toSpeak = area.value;
-    console.log(toSpeak);
-    const utter = new SpeechSynthesisUtterance(toSpeak);
-    synth.speak(utter);
-  });
-}
+
 
 window.onload = () => {
-  initRecog();
-  speechSynth();
+
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('select');
+  
+  const speak = text => {
+    const voices = window.speechSynthesis.getVoices();
+    
+    var utterThis = new SpeechSynthesisUtterance(text);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(let i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+  };
+
+  synth.onvoiceschanged = () => {
+    const voices = window.speechSynthesis.getVoices();
+    console.log(voices);
+    for(let i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  const speechSynth = () => {
+    const area = document.querySelector('textarea');
+    const button = document.getElementById('speak');
+    button.addEventListener('click', () => {
+      speak(area.value);
+    });
+  }
+  
+ 
+  initRecog(speak);
+  speechSynth(speak);
 }
