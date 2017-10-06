280b0fcbe8dbdbad349c3db24143ff103b20cf51
Harry <fitzgerald_harry@network.lilly.com>
2017-10-06 16:02:35 +0100

    add eslint and gulp and jsdoc

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const jsdoc = require('gulp-jsdoc3');
+const jsdocConfig = require('./jsdoc.config.json');
+/**
+ * generate some JSDoc for every js file in src/
+ * @type {Function}
+ */
+gulp.task('doc', cb => {
+  gulp.src(['*.js']).pipe(jsdoc(jsdocConfig, cb));
+});
+
+/**
+ * Watch the files for changes. If they change, re-babel-ify the files in src/
+ * Also regenerate the documentation.
+ */
+gulp.task('watch', () => {
+  gulp.watch(['*.js'], ['doc']);
+});
+
+/**
+ * When Gulp is run, we want to transpile it and generate the docs
+ * We then watch for any changes.
+ */
+gulp.task('default', ['doc']);
